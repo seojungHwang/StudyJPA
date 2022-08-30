9f1eda61ec9159f1399a13ec5b1c78d9d51a0d72
seojungHwang <tjwjd101@gmail.com>
2022-08-30 22:03:31 +0900

    JpaMain Update

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -38,11 +38,22 @@ public class JpaMain {
           //수정
           Member findMember = em.find(Member.class, 1L);
           findMember.setName("Hello Update");
+          //em.persist(findMember); -> 수정시에는 필요 없음
 
           // em.remove(findMember); 삭제
 
-        //  em.persist(member); //DB 저장
-          tx.commit();
+            //  em.persist(member); //실질 DB 저장은 아님  //영속상태
+            // em.flush(); //쿼리에 바로 반영
+            //em.detach(member); //영속성 컨텍스트 관리x / 특정 엔티티만 준영속 상태로 전환
+
+           /*  비영속
+          속Member member = new Member();
+            member.setId(100L);*/
+
+
+
+
+          tx.commit();  //DB 쿼리 날라감
         } catch (Exception e){
           tx.rollback();
         }finally {
